a01f5d0c12ae77e49e9203566c6463c991982020
George Arnold <george.arnold.88@gmail.com>
2020-01-03 18:01:25 -0500

    fixed accidental editing of app.test.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, {Component from 'react';
 
-function App() {
+class App extends Component {
+  render() {
   return (
     <main className='App'>
-      {/* content goes here */}
+    {/* Pass callback function to change state when user enters information */}
+     <UserInput />
+    {/* Pass data from state with which books are filtered*/}
+     <Books />
     </main>
   );
 }
-
+}
 export default App;
